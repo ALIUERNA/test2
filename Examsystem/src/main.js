417fc358421d217912ebae7e6fdf25e8a31aaf9b
ALIUERNA <2490097529@qq.com>
2025-04-15 10:01:42 +0800

    Initial commit

---
+++ Examsystem/src/main.js
@@ -0,0 +1,17 @@
+import { createApp } from 'vue';
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'; // 引入图标库
+import App from './App.vue';
+
+const app = createApp(App);
+
+// 全局注册图标组件
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component);
+}
+
+app.use(ElementPlus);
+app.mount('#app');
+
+
